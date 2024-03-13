13922ea435b6257de4a196d713858b70b1a03a68
Sam Lui <hhu354@aucklanduni.ac.nz>
2024-03-13 15:58:38 +1300

    Added player animation

--- Player/player.gd
+++ Player/player.gd
@@ -5,21 +5,45 @@ extends CharacterBody2D
 @export var friction = 256
 @export var gravity = 200
 @export var jump_force = 128
-@export var max_fall_speed = 80
+@export var max_fall_velocity = 128
+
+@onready var animation_player = $AnimationPlayer
+@onready var sprite_2d = $Sprite2D
 
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y = move_toward(velocity.y, max_fall_speed, gravity * delta)
-	
+	apply_gravity(delta)
 	var input_axis = Input.get_axis("ui_left", "ui_right")
-	
-	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, input_axis * max_speed, acceleration * delta)
+	if is_moving(input_axis):
+		apply_acceleration(delta, input_axis)
 	else:
-		velocity.x = move_toward(velocity.x,  0, friction * delta)
-	
+		apply_friction(delta)
+	jump_check()
+	update_animations(input_axis)
+	move_and_slide()
+
+func is_moving(input_axis):
+	return input_axis != 0
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y = move_toward(velocity.y, max_fall_velocity, gravity * delta)
+
+func apply_acceleration(delta, input_axis):
+	velocity.x = move_toward(velocity.x, input_axis * max_speed, acceleration * delta)
+
+func apply_friction(delta):
+	velocity.x = move_toward(velocity.x,  0, friction * delta)
+
+func jump_check():
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = -jump_force
-		
-	move_and_slide()
+
+func update_animations(input_axis):
+	if is_moving(input_axis):
+		animation_player.play("run")
+		sprite_2d.scale.x = sign(input_axis)
+	else:
+		animation_player.play("idle")
+	if not is_on_floor():
+		animation_player.play("jump")
