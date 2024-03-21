19c80f6d6f1fddea9305fba2ae9c3378ec7b0584
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2024-03-21 20:46:29 +1300

    enemy sprite flip

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 @export var acceleration = 150
 @export var max_speed = 60
 
+func is_moving(input_axis):
+	return input_axis != 0
+
 func _physics_process(delta):
 	var player = MainInstance.player
 	if player is CharacterBody2D:
@@ -12,3 +15,7 @@ func move_toward_position(target_position,delta):
 	var direction = global_position.direction_to(target_position)
 	velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
 	move_and_slide()
+	
+func update_animations(input_axis):
+	if is_moving(input_axis):
+		$PirateGhostPlaceholder.scale.x = sign(input_axis)
