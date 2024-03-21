79662e047d310cb21097875afc4a80a79020b3a7
Sam Lui <hhu354@aucklanduni.ac.nz>
2024-03-21 20:52:41 +1300

    Ghost rotation

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -3,12 +3,15 @@ extends CharacterBody2D
 @export var acceleration = 150
 @export var max_speed = 60
 
+@onready var pirate_ghost_placeholder = $PirateGhostPlaceholder
+
 func is_moving(input_axis):
 	return input_axis != 0
 
 func _physics_process(delta):
 	var player = MainInstance.player
 	if player is CharacterBody2D:
+		flip_towards_player(player)
 		move_toward_position(player.global_position,delta)
 			
 func move_toward_position(target_position,delta):
@@ -16,6 +19,13 @@ func move_toward_position(target_position,delta):
 	velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
 	move_and_slide()
 	
-func update_animations(input_axis):
-	if is_moving(input_axis):
-		$PirateGhostPlaceholder.scale.x = sign(input_axis)
+#func update_animations(input_axis):
+	#if is_moving(input_axis):
+		#$PirateGhostPlaceholder.scale.x = sign(input_axis)
+
+func flip_towards_player(player):
+	if player.global_position.x < global_position.x:
+		pirate_ghost_placeholder.flip_h = true
+	else:
+		pirate_ghost_placeholder.flip_h = false
+	
--- Obstacles/Spikes.gd
+++ Obstacles/Spikes.gd
@@ -10,5 +10,6 @@ func _ready():
 
 
 func _on_body_entered(body):
+	print("entered")
 	if body.has_method("death"):
 		body.death()
